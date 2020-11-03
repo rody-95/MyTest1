c43326c6a18754a127c63825e5a7507b6431a048
xinlin <1152700831@qq.com>
2020-11-03 15:06:43 +0800

    更换IDEA

--- RedisTest.py
+++ RedisTest.py
@@ -1,4 +1,5 @@
 #redis数据库
+#git项目管理
 import redis
 r = redis.StrictRedis(host='127.0.0.1',port='6379',db='0',password='94995',decode_responses=True)
 r.set('token',"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwbGF0Q29kZSI6IjAiLCJhY2NJbmZvSWQiOjEyNTQyNTY0NTMxNjg0ODAyNTYsInBob25lIjoiMTUxMTMxOTUzNDMiLCJleHRJZCI6MCwiZXhwIjoxNjA0OTE1MjYwLCJ1c2VySWQiOjE0NTcxNjg2LCJvcmdJZCI6Im51bGwiLCJlbWFpbCI6Inhpbmxpbi56aGFuZ0BtZWRiYW5rcy5jbiJ9.DT9yQBeMxgRBfJNpi8aW6j8cO5Hyatcfrb1gns59lG4")
